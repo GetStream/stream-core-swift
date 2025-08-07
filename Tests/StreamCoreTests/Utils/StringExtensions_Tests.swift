6f4776201ec17d12df1d0b21a5408bc01e5d8427
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2025-08-07 10:33:32 +0200

    Added tests

---
+++ Tests/StreamCoreTests/Utils/StringExtensions_Tests.swift
@@ -0,0 +1,17 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+@testable import StreamCore
+import XCTest
+
+final class StringExtensions_Tests: XCTestCase {
+
+    func test_Levenshtein() throws {
+        XCTAssertEqual("".levenshtein(""), "".levenshtein(""))
+        XCTAssertEqual("".levenshtein(""), 0)
+        XCTAssertEqual("a".levenshtein(""), 1)
+        XCTAssertEqual("".levenshtein("a"), 1)
+        XCTAssertEqual("tommaso".levenshtein("ToMmAsO"), 4)
+    }
+}
