7a594e05e6fb1dd40309fb96c20dffe9ad14171e
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2025-08-25 23:54:49 +0200

    Updated disconnect logic for WS

--- Sources/StreamCore/WebSocket/Client/ConnectionRecoveryHandler.swift
+++ Sources/StreamCore/WebSocket/Client/ConnectionRecoveryHandler.swift
@@ -205,7 +205,7 @@ private extension DefaultConnectionRecoveryHandler {
         let state = webSocketClient.connectionState
         
         switch state {
-        case .connecting, .authenticating, .connected:
+        case .connecting, .authenticating, .connected, .disconnecting:
             log.debug("Will disconnect automatically from \(state) state", subsystems: .webSocket)
             
             return true
