426dd1f69b680efd6aa1a93505ce5df1d15a2f66
Martin Mitrevski <martinmitrevski.oh@gmail.com>
2025-08-25 17:52:27 +0200

    Added publisher next value

---
+++ Sources/StreamCore/Utils/Publisher+AsyncStream.swift
@@ -0,0 +1,89 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+@preconcurrency import Combine
+import Foundation
+
+public extension Publisher where Output: Sendable {
+
+    /// Converts the current publisher into an `AsyncStream` of its output.
+    ///
+    /// This allows you to consume any Combine publisher using Swift's `for await`
+    /// syntax, providing a bridge between Combine and Swift Concurrency.
+    ///
+    /// - Returns: An `AsyncStream` emitting values of the publisher as they arrive.
+    func eraseAsAsyncStream() -> AsyncStream<Output> {
+        AsyncStream { continuation in
+            let cancellable = self.sink(
+                receiveCompletion: { _ in
+                    continuation.finish()
+                },
+                receiveValue: {
+                    continuation.yield($0)
+                }
+            )
+
+            continuation.onTermination = { @Sendable _ in
+                cancellable.cancel()
+            }
+        }
+    }
+
+    func firstValue(
+        file: StaticString = #file,
+        line: UInt = #line
+    ) async throws -> Output {
+        if #available(iOS 15.0, *) {
+            for try await value in self.values {
+                return value
+            }
+        } else {
+            for try await value in eraseAsAsyncStream() {
+                return value
+            }
+        }
+
+        throw ClientError("Task produced no value.", file, line)
+    }
+
+    func firstValue(
+        timeoutInSeconds: TimeInterval,
+        file: StaticString = #file,
+        function: StaticString = #function,
+        line: UInt = #line
+    ) async throws -> Output {
+        nonisolated(unsafe) let selfReference = self
+        return try await Task(
+            timeoutInSeconds: timeoutInSeconds,
+            file: file,
+            function: function,
+            line: line
+        ) {
+            try await selfReference.firstValue(file: file, line: line)
+        }.value
+    }
+
+    func nextValue(
+        dropFirst: Int = 0,
+        timeout: TimeInterval? = nil,
+        file: StaticString = #fileID,
+        function: StaticString = #function,
+        line: UInt = #line
+    ) async throws -> Output {
+        let publisher = dropFirst > 0
+            ? self.dropFirst(dropFirst).eraseToAnyPublisher()
+            : eraseToAnyPublisher()
+
+        if let timeout {
+            return try await publisher.firstValue(
+                timeoutInSeconds: timeout,
+                file: file,
+                function: function,
+                line: line
+            )
+        } else {
+            return try await publisher.firstValue()
+        }
+    }
+}
---
+++ Sources/StreamCore/Utils/Task+Timeout.swift
@@ -0,0 +1,52 @@
+//
+// Copyright © 2025 Stream.io Inc. All rights reserved.
+//
+
+import Foundation
+
+/// An extension to the `Task` type that adds timeout functionality.
+extension Task where Failure == any Error {
+    @discardableResult
+    init(
+        priority: TaskPriority? = nil,
+        /// New: a timeout property to configure how long a task may perform before failing with a timeout error.
+        timeoutInSeconds: TimeInterval,
+        file: StaticString = #fileID,
+        function: StaticString = #function,
+        line: UInt = #line,
+        operation: @Sendable @escaping () async throws -> Success
+    ) {
+        self = Task(priority: priority) {
+            try await withThrowingTaskGroup(of: Success.self) { group in
+
+                /// Add the operation to perform as the first task.
+                _ = group.addTaskUnlessCancelled {
+                    try await operation()
+                }
+
+                if timeoutInSeconds > 0, timeoutInSeconds <= TimeInterval(UInt64.max) {
+                    /// Add another task to trigger the timeout if it finishes earlier than our first task.
+                    _ = group.addTaskUnlessCancelled { () -> Success in
+                        try await Task<Never, Never>.sleep(nanoseconds: UInt64(timeoutInSeconds * 1_000_000_000))
+                        throw ClientError("Operation timed out", file, line)
+                    }
+                } else {
+                    log.warning("Invalid timeout:\(timeoutInSeconds) was passed to Task.timeout. Task will timeout immediately.")
+                    throw ClientError("Operation timed out", file, line)
+                }
+
+                /// We need to deal with an optional, even though we know it's not optional.
+                /// This is default for task groups to account for when there aren't any pending tasks.
+                /// Awaiting on an empty group immediately returns 'nil' without suspending.
+                guard let result = try await group.next() else {
+                    throw ClientError("Task produced no value", file, line)
+                }
+
+                /// If we reach this, it means we have a value before the timeout.
+                /// We cancel the group, which means just cancelling the timeout task.
+                group.cancelAll()
+                return result
+            }
+        }
+    }
+}
