96346425d150f75d97974f39f1adaaefbf689833
Toomas Vahter <toomas.vahter@getstream.io>
2025-10-31 14:49:15 +0200

    Fix tests

--- Tests/StreamCoreTests/WebSockets/WebSocketClient/WebSocketClient_Tests.swift
+++ Tests/StreamCoreTests/WebSockets/WebSocketClient/WebSocketClient_Tests.swift
@@ -50,7 +50,7 @@ final class WebSocketClient_Tests: XCTestCase, @unchecked Sendable {
             eventNotificationCenter: eventNotificationCenter,
             webSocketClientType: .coordinator,
             environment: environment,
-            connectURL: connectURL
+            connectRequest: URLRequest(url: connectURL)
         )
 
         connectionId = UUID().uuidString
@@ -226,7 +226,6 @@ final class WebSocketClient_Tests: XCTestCase, @unchecked Sendable {
             [
                 .connecting,
                 .authenticating,
-                .connected(healthCheckInfo: healthCheckInfo),
                 .connected(healthCheckInfo: healthCheckInfo)
             ]
         )
